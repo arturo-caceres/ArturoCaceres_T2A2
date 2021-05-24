65f125ad97b8bed0eadfc60d879fea5965787bc2
arturo-caceres <arturo.caceres02383@gmail.com>
2021-05-24 16:49:59 +1000

    Redirecting root_ path to plants

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root "plants#home"
 
+  get "user_root", to: redirect("/plants"), as: :user_root
+
   #Route to the about page
   get "/about", to: "about#index"
 
