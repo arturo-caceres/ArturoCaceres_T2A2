9fbb1e76b2ad0fa0101ad7b19b2aff613b99aa6d
arturo-caceres <arturo.caceres02383@gmail.com>
2021-05-26 21:50:02 +1000

    Debugging migrations

--- db/migrate/20210523131140_add_user_id_to_plants.rb
+++ db/migrate/20210523131140_add_user_id_to_plants.rb
@@ -1,5 +1,5 @@
 class AddUserIdToPlants < ActiveRecord::Migration[6.1]
   def change
-    add_reference(:plants, :buyer, foreign_key: { to_table: :users })
+    add_reference :plants, :user, foreign_key: true
   end
 end
